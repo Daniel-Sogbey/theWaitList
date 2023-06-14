1de58907e48065403cc6ad576a1dd8ae1d5b4124
Foster Addo <addofoster047@gmail.com>
2023-06-13 23:03:13 -0700

    added user data collection and validated user input.

--- public/js/script.js
+++ public/js/script.js
@@ -2,18 +2,55 @@ let container = document.querySelector(".container");
 let popup = document.querySelector("#popup");
 let btn = document.querySelector(".btn");
 let close = document.querySelector(".close");
+let form = document.querySelector("#myForm");
+let nameInput = document.querySelector("#name");
+let emailInput = document.querySelector("#email");
 
-popup.style.display = "none"
+popup.style.display = "none";
 
-btn.addEventListener("click", function(){
-    container.classList.toggle("active");
+btn.addEventListener("click", function(event) {
+  event.preventDefault();
 
-    popup.style.display = "block";
+  // Checking validity of name and email fields
+  if (nameInput.value.trim() === "") {
+    alert("Please enter your name.");
+    return;
+  }
+  if (emailInput.value.trim() === "") {
+    alert("Please enter your email address.");
+    return;
+  }
+
+  // Checking validity of email format using a regular expression
+  var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(emailInput.value)) {
+    alert("Please enter a valid email address.");
+    return;
+  }
+
+  // Log user input to the console
+  console.log("Name:", nameInput.value);
+  console.log("Email:", emailInput.value);
+
+  //Storing the user input in a userData array.
+  var userData = {
+    name: nameInput.value,
+    email: emailInput.value
+  };
+
+  // Showing the pop-up message
+  container.classList.toggle("active");
+  popup.style.display = "block";
+
 });
 
-close.addEventListener("click", function(){
-    popup.style.display = "none"
-    container.classList.remove("active");
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+});
+
+close.addEventListener("click", function() {
+  popup.style.display = "none";
+  container.classList.remove("active");
 });
 
 
